7f43af406472416ce2d0598b513c97950c3ed1d8
Oleg Pak <pak.o99@mail.ru>
2024-04-12 18:27:43 +0300

    added TaskList view and template

--- todo_list/tasklist/views.py
+++ todo_list/tasklist/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def tasklist(request):
-    return HttpResponse('ToDo list')
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
