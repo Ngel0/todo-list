3597461e96392f35b284c8b1dd9ca7e3ac2e3c9d
Oleg Pak <pak.o99@mail.ru>
2024-04-16 13:20:03 +0300

    added SignUpView

--- todo_list/accounts/views.py
+++ todo_list/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
