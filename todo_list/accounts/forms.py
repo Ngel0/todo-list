bb407ee9a7ea2e892169cfd2fdb0326a9bba2307
Oleg Pak <pak.o99@mail.ru>
2024-04-16 20:28:10 +0300

    removed some help text

--- todo_list/accounts/forms.py
+++ todo_list/accounts/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = None
+        self.fields['password2'].help_text = None
+
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
