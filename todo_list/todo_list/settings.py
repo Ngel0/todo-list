579a697aff5529a94fa8d04e75f38e4a75d8d792
Oleg Pak <pak.o99@mail.ru>
2024-04-22 00:41:21 +0300

    changed .env file path

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path='../.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
