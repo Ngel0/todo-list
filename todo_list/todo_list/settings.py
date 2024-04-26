ca747f6c0f1a3d6a0a7d05de33d70f0dcb8ccfd3
Oleg Pak <pak.o99@mail.ru>
2024-04-26 22:18:11 +0300

    added docker compose for postgres, pgadmin and django

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 from dotenv import load_dotenv
 import os
 
-load_dotenv(dotenv_path='../.env')
+load_dotenv(dotenv_path='../../.env.local')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
