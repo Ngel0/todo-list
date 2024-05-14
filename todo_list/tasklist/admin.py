4ac6a62c0c59db8305c6eb5d9f24325ece9eb816
Oleg Pak <pak.o99@mail.ru>
2024-05-14 08:07:10 +0300

    added a TaskList model

--- todo_list/tasklist/admin.py
+++ todo_list/tasklist/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Task
+from .models import TaskList, Task
 
+admin.site.register(TaskList)
 admin.site.register(Task)
--- todo_list/tasklist/models.py
+++ todo_list/tasklist/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class TaskList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
@@ -9,6 +17,7 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    todo_list = models.ForeignKey(TaskList, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
